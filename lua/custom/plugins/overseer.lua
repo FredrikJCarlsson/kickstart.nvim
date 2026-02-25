e9fc787c236516ebd95c3d7da8bedc3f52ea37b0
Fredrik Carlsson <fredrik.j.carlsson@unipower.se>
2026-02-25 14:24:50 +0100

    Added overseer

---
+++ lua/custom/plugins/overseer.lua
@@ -0,0 +1,65 @@
+return {
+  'stevearc/overseer.nvim',
+  cmd = {
+    'OverseerOpen',
+    'OverseerClose',
+    'OverseerToggle',
+    'OverseerSaveBundle',
+    'OverseerLoadBundle',
+    'OverseerDeleteBundle',
+    'OverseerRunCmd',
+    'OverseerRun',
+    'OverseerInfo',
+    'OverseerBuild',
+    'OverseerQuickAction',
+    'OverseerTaskAction',
+    'OverseerClearCache',
+  },
+  opts = {
+    dap = false,
+    task_list = {
+      bindings = {
+        ['<C-h>'] = false,
+        ['<C-j>'] = false,
+        ['<C-k>'] = false,
+        ['<C-l>'] = false,
+      },
+    },
+    form = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    confirm = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+    task_win = {
+      win_opts = {
+        winblend = 0,
+      },
+    },
+  },
+  config = function()
+    require('overseer').setup {
+      templates = {
+        'builtin',
+        'user.zig_build',
+        'user.msbuild_debug',
+        'user.syncAndStartGdb',
+        -- "user.UP2210V3",
+      },
+    }
+  end,
+  -- stylua: ignore
+  keys = {
+    { "<leader>ow", "<cmd>OverseerToggle<cr>",      desc = "Task list" },
+    { "<leader>oo", "<cmd>OverseerRun<cr>",         desc = "Run task" },
+    { "<leader>oq", "<cmd>OverseerQuickAction<cr>", desc = "Action recent task" },
+    { "<leader>oi", "<cmd>OverseerInfo<cr>",        desc = "Overseer Info" },
+    { "<leader>ob", "<cmd>OverseerBuild<cr>",       desc = "Task builder" },
+    { "<leader>ot", "<cmd>OverseerTaskAction<cr>",  desc = "Task action" },
+    { "<leader>oc", "<cmd>OverseerClearCache<cr>",  desc = "Clear cache" },
+  },
+}
