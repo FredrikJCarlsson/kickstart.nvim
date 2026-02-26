e4c2a4aba500b970d42556bf31232caef1dfa527
Fredrik Carlsson <fredrik.j.carlsson@unipower.se>
2026-02-26 11:37:55 +0100

    Fixed some more with icons and also copilot nes

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -1,13 +1,41 @@
 return {
   'zbirenbaum/copilot.lua',
   requires = {
-    'copilotlsp-nvim/copilot-lsp', -- (optional) for NES functionality
+    'copilotlsp-nvim/copilot-lsp',
+    init = function()
+      vim.g.copilot_nes_debounce = 500
+      vim.lsp.enable 'copilot_ls'
+      vim.keymap.set('n', '<tab>', function()
+        local bufnr = vim.api.nvim_get_current_buf()
+        local state = vim.b[bufnr].nes_state
+        if state then
+          -- Try to jump to the start of the suggestion edit.
+          -- If already at the start, then apply the pending suggestion and jump to the end of the edit.
+          local _ = require('copilot-lsp.nes').walk_cursor_start_edit()
+            or (require('copilot-lsp.nes').apply_pending_nes() and require('copilot-lsp.nes').walk_cursor_end_edit())
+          return nil
+        else
+          -- Resolving the terminal's inability to distinguish between `TAB` and `<C-i>` in normal mode
+          return '<C-i>'
+        end
+      end, { desc = 'Accept Copilot NES suggestion', expr = true })
+    end,
   },
   cmd = 'Copilot',
   build = ':Copilot auth',
   event = 'BufReadPost',
-  config = function(_, opts)
-    require('copilot').setup(opts)
+  config = function()
+    require('copilot').setup {
+      nes = {
+        enabled = true,
+        move_count_threshold = 3,
+        keymap = {
+          -- accept_and_goto = '<S-Tab>',
+          -- accept = false,
+          dismiss = '<Esc>',
+        },
+      },
+    }
     require('copilot_cmp').setup()
   end,
   opts = {
--- lua/custom/plugins/which-key.lua
+++ lua/custom/plugins/which-key.lua
@@ -1,6 +1,7 @@
 return {
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     event = 'VimEnter',
     opts = {
       -- delay between pressing a key and opening which-key (milliseconds)
@@ -10,11 +11,26 @@ return {
       -- Document existing key chains
       spec = {
         { '<leader>g', group = 'Git', icon = '' },
-        { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
-        { '<leader>t', group = '[T]oggle' },
-        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>c', group = 'Code', icon = '' },
+        { '<leader>f', group = 'Find', icon = '󰮗' },
+        { '<leader>b', group = 'Buffer', icon = '󰈙' },
+        { '<leader>gh', group = 'Git Hunk', mode = { 'n', 'v' } },
+        { '<leader>s', group = 'Search', mode = { 'n', 'v' } },
+        { '<leader>t', group = 'Toggle' },
+        { '<leader>u', group = 'UI', icon = '󰹑' },
+
         { '<C-s>', '<cmd>w<cr>', desc = 'Save buffer', mode = { 'n', 'i', 'v' } },
         { '<leader>qq', '<cmd>qa<cr>', desc = 'Quit all' },
+
+        -- snacks
+        { '<leader><space>', desc = '󰈞' },
+        { '<leader>,', icon = '' },
+        { '<leader>/', icon = '' },
+        { '<leader>:', icon = '' },
+        { '<leader>n', icon = '' },
+        { '<leader>e', icon = '' },
+        { '<leader>fb', icon = '' },
+        { '<leader>fc', icon = '' },
       },
     },
   },
